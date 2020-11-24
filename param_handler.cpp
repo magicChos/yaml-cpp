9bb91dbfd81269cf36d01f764c330f78e049476f
hanshuobest <hscoder@163.com>
2020-11-24 21:02:26 +0800

    2020-11-24

--- param_handler.cpp
+++ param_handler.cpp
@@ -1,83 +1,58 @@
-//
-// Created by dyq on 2020/10/23.
-//
 
 #include "param_handler.h"
-// #include "ace/common/param_handler.h"
 #include <iostream>
 
-namespace ace
-{
-    namespace common
-    {
+namespace ace {
+namespace common {
 
-        ParamHandler::ParamHandler(const std::string &file_name)
-        {
-            try
-            {
-                config_ = YAML::LoadFile(file_name);
-                fileLoaded = true;
-            }
-            catch (std::exception &e)
-            {
-                fileLoaded = false;
-            }
-        }
+ParamHandler::ParamHandler(const std::string &file_name) {
+  try {
+    config_ = YAML::LoadFile(file_name);
+    fileLoaded = true;
+  } catch (std::exception &e) {
+    fileLoaded = false;
+  }
+}
 
-        ParamHandler::~ParamHandler() {}
+ParamHandler::~ParamHandler() {}
 
-        bool ParamHandler::GetString(const std::string &key, std::string &str_value)
-        {
-            try
-            {
-                str_value = config_[key].as<std::string>();
-            }
-            catch (std::exception &e)
-            {
-                return false;
-            }
-            return true;
-        }
+bool ParamHandler::GetString(const std::string &key, std::string &str_value) {
+  try {
+    str_value = config_[key].as<std::string>();
+  } catch (std::exception &e) {
+    return false;
+  }
+  return true;
+}
 
-        bool ParamHandler::GetString(const std::string &category, const std::string &key, std::string &str_value)
-        {
-            try
-            {
-                str_value = config_[category][key].as<std::string>();
-            }
-            catch (std::exception &e)
-            {
-                return false;
-            }
-            return true;
-        }
+bool ParamHandler::GetString(const std::string &category,
+                             const std::string &key, std::string &str_value) {
+  try {
+    str_value = config_[category][key].as<std::string>();
+  } catch (std::exception &e) {
+    return false;
+  }
+  return true;
+}
 
-        bool ParamHandler::GetBoolean(const std::string &key, bool &bool_value)
-        {
-            try
-            {
-                bool_value = config_[key].as<bool>();
-            }
-            catch (std::exception &e)
-            {
-                return false;
-            }
-            return true;
-        }
+bool ParamHandler::GetBoolean(const std::string &key, bool &bool_value) {
+  try {
+    bool_value = config_[key].as<bool>();
+  } catch (std::exception &e) {
+    return false;
+  }
+  return true;
+}
 
-        bool ParamHandler::GetInteger(const std::string &key, int &int_value)
-        {
-            try
-            {
-                int_value = config_[key].as<int>();
-            }
-            catch (std::exception &e)
-            {
-                return false;
-            }
+bool ParamHandler::GetInteger(const std::string &key, int &int_value) {
+  try {
+    int_value = config_[key].as<int>();
+  } catch (std::exception &e) {
+    return false;
+  }
 
-            return true;
-        }
+  return true;
+}
 
-    } // namespace common
+} // namespace common
 } // namespace ace
--- read_calibration.cpp
+++ read_calibration.cpp
@@ -1,67 +1,59 @@
-#include <iostream>
-#include <yaml-cpp/yaml.h>
+
 #include <fstream>
-#include <vector>
+#include <iostream>
 #include <opencv2/opencv.hpp>
+#include <vector>
+#include <yaml-cpp/yaml.h>
 
 using namespace std;
 using namespace cv;
 
 /***************** Mat转vector **********************/
-template <typename _Tp>
-vector<_Tp> convertMat2Vector(const Mat &mat)
-{
-    return (vector<_Tp>)(mat.reshape(1, 1)); //通道数不变，按行转为一行
+template <typename _Tp> vector<_Tp> convertMat2Vector(const Mat &mat) {
+  return (vector<_Tp>)(mat.reshape(1, 1)); //通道数不变，按行转为一行
 }
 
 /****************** vector转Mat *********************/
 template <typename _Tp>
-cv::Mat convertVector2Mat(std::vector<_Tp> v, int channels, int rows)
-{
-    cv::Mat mat = cv::Mat(v);                           //将vector变成单列的mat
-    cv::Mat dest = mat.reshape(channels, rows).clone(); //PS：必须clone()一份，否则返回出错
-    return dest;
+cv::Mat convertVector2Mat(std::vector<_Tp> v, int channels, int rows) {
+  cv::Mat mat = cv::Mat(v); //将vector变成单列的mat
+  cv::Mat dest =
+      mat.reshape(channels, rows).clone(); // PS：必须clone()一份，否则返回出错
+  return dest;
 }
 
-
-
-
-int main(int argc, char **argv)
-{
-    YAML::Node config;
-    try
-    {
-        if (argc != 2)
-        {
-            std::cerr << "usage: exe config.yaml" << std::endl;
-            return 0;
-        }
-        std::cout << "file: " << argv[1] << std::endl;
-        config = YAML::LoadFile(argv[1]);
+int main(int argc, char **argv) {
+  YAML::Node config;
+  try {
+    if (argc != 2) {
+      std::cerr << "usage: exe config.yaml" << std::endl;
+      return 0;
     }
-    catch (YAML::BadFile &e)
-    {
-        std::cerr << "read error!" << e.what() << std::endl;
-        return -1;
-    }
-    std::cout << "Node type: " << config.Type() << std::endl;
+    std::cout << "file: " << argv[1] << std::endl;
+    config = YAML::LoadFile(argv[1]);
+  } catch (YAML::BadFile &e) {
+    std::cerr << "read error!" << e.what() << std::endl;
+    return -1;
+  }
+  std::cout << "Node type: " << config.Type() << std::endl;
 
-    std::vector<float> cameraMat = config["CameraMat"]["data"].as<std::vector<float>>();
+  std::vector<float> cameraMat =
+      config["CameraMat"]["data"].as<std::vector<float>>();
 
-    for (auto i : cameraMat)
-    {
-        std::cout << i << "\t";
-    }
-    std::cout << std::endl;
+  for (auto i : cameraMat) {
+    std::cout << i << "\t";
+  }
+  std::cout << std::endl;
 
-    std::vector<float> distCoeff = config["DistCoeff"]["data"].as<std::vector<float>>();
+  std::vector<float> distCoeff =
+      config["DistCoeff"]["data"].as<std::vector<float>>();
 
-    cv::Mat distCoeffMat = cv::Mat(distCoeff);
-    std::cout << "-----------------------------------------" << std::endl;
-    std::cout << distCoeffMat << std::endl;
+  cv::Mat distCoeffMat = cv::Mat(distCoeff);
+  std::cout << "-----------------------------------------" << std::endl;
+  std::cout << distCoeffMat << std::endl;
 
-    cv::Mat camera_mat = convertVector2Mat<float>(cameraMat, 1, 3);
-    std::cout << camera_mat << std::endl;
+  cv::Mat camera_mat = convertVector2Mat<float>(cameraMat, 1, 3);
+  std::cout << camera_mat << std::endl;
 
-    return 1;
+  return 1;
 }
